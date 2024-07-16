34c7a05cbb8214fbcc222e58a8b04aa5fdb03b77
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-07-17 01:31:26 +0530

    Split out some components

---
+++ src/lib/achievements.ts
@@ -0,0 +1,17 @@
+export const achievements = [
+	{
+		src: "./images/boeing.png",
+		heading: "BOEING Aeromodelling IITM",
+		description: "Stood in the top 20 of the Boeing National Aeromodelling Competition. This stands as a testament to our relentless pursuit of excellence."
+	},
+	{
+		src: "./images/wow.jpg",
+		heading: "Wings of Wonder",
+		description: "Wings of Wonder is a program aiming to expose young students to the world of model aircraft, sowing the seeds of interest at a young age."
+	},
+	{
+		src: "./images/aci.jpg",
+		heading: "Air Crash Investigation",
+		description: "Our 8th mile event, a testament to the commitment to safer skies, and an opportunity to fortify the foundations of aviation safety worldwide."
+	}
+];
