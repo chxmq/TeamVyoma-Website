f4a8f8628ad9ba7a87514c58bf62980db60b860c
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-07-17 14:00:07 +0530

    More responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'vyoma-blue': '#30C5DC'
+		}
 	},
 	plugins: [],
 }
