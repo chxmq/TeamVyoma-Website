25d06a8d0ca23ecc47a34f52e3b51fd8e73cb24c
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-07-22 18:26:43 +0530

    Achievements

--- src/lib/achievements.ts
+++ src/lib/achievements.ts
@@ -5,13 +5,33 @@ export const achievements = [
 		description: "Stood in the top 20 of the Boeing National Aeromodelling Competition. This stands as a testament to our relentless pursuit of excellence."
 	},
 	{
+		src: "./images/aci.jpg",
+		heading: "Air Crash Investigation",
+		description: "Our 8th mile event, a testament to the commitment to safer skies, and an opportunity to fortify the foundations of aviation safety worldwide."
+	},
+	{
+		src: "./images/nacdec.png",
+		heading: "NACDEC",
+		description: "All three teams from Team Vyoma stood in the top 10%. The objective was to design an aircraft capable of carrying and dropping a heavy payload"
+	},
+	{
+		src: "./images/guppy.png",
+		heading: "Aerodominator 7.0",
+		description: "Developed a remote-controlled aircraft capable of carrying medical supplies. Stood in the 8th and 13th positions."
+	},
+	{
+		src: "./images/dbf.png",
+		heading: "DBF 2019",
+		description: "Designed a multi-purpose aircraft supporting carrier operations with specific design parameters. Stood in the 41st position out of 113 teams."
+	},
+	{
 		src: "./images/wow.jpg",
 		heading: "Wings of Wonder",
 		description: "Wings of Wonder is a program aiming to expose young students to the world of model aircraft, sowing the seeds of interest at a young age."
 	},
 	{
-		src: "./images/aci.jpg",
-		heading: "Air Crash Investigation",
-		description: "Our 8th mile event, a testament to the commitment to safer skies, and an opportunity to fortify the foundations of aviation safety worldwide."
+		src: "./images/skysparks.png",
+		heading: "Skysparks Techkriti",
+		description: "Designed an aircraft suitable for crop dusting, firefighting, surveying, and medicinal cargo deliveries."
 	}
 ];
