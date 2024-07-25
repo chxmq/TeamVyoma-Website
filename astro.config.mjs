66e4c8034e926997ac2c670806462cfb3c04da3c
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-07-25 19:14:28 +0530

    Change canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vyoma.shrishdeshpande.com',
+  site: 'https://teamvyoma.in',
   integrations: [tailwind(), sitemap()]
 });
